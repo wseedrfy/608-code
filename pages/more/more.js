e2300021320e73b6c0aeaf02d75f2052469b72ec
ChanWingKei233 <89628135+ChanWingKei233@users.noreply.github.com>
2021-11-01 11:29:39 +0800

    校园圈

--- pages/more/more.js
+++ pages/more/more.js
@@ -7,42 +7,7 @@ Page({
    */
   data: {
    
-    noramalData: [{
-        "Cover": "http://dashus.oss-cn-shenzhen.aliyuncs.com/DefaultImage/Game/20190306144842/1001.png",
-        "Title":"标题标题标题标题标题标题",
-        "Text":"文字文字文字文字文字文字",
-        "CoverHeight": 340,
-        "CoverWidth": 270
-      },
-      {
-        "Cover": "http://dashus.oss-cn-shenzhen.aliyuncs.com/DefaultImage/Game/20190313090409/完美9.png",
-        "Title":"标题标题标题标题标题标题",
-        "Text":"文字文字文字文字文字文字",
-        "CoverHeight": 871,
-        "CoverWidth": 672
-      },
-      {
-        "Cover": "http://dashus.oss-cn-shenzhen.aliyuncs.com/DefaultImage/Game/20190313090409/完美9.png",
-        "Title":"标题标题标题标题标题标题",
-        "Text":"文字文字文字文字文字文字",
-        "CoverHeight": 871,
-        "CoverWidth": 672
-      },
-      {
-        "Cover": "https://img.zcool.cn/community/01743a594097d3a8012193a3aa31eb.jpg@1280w_1l_2o_100sh.jpg",
-        "Title":"标题标题标题标题标题标题",
-        "Text":"文字文字文字文字文字文字",
-        "CoverHeight": 930,
-        "CoverWidth": 672
-      },
-      {
-        "Cover": "https://img.zcool.cn/community/01e2525c235064a8012029ac7836fa.jpg@1280w_1l_2o_100sh.jpg",
-        "Title":"标题标题标题标题标题标题",
-        "Text":"文字文字文字文字文字文字",
-        "CoverHeight": 571,
-        "CoverWidth": 672
-      }
-    ],
+    noramalData: [],
     tabitem:[{
         id:0,
         title:"日常",
@@ -70,13 +35,18 @@ Page({
     ],
     leftList: [],
     rightList: [],
-    Title:' ',
-    Text:' ',
+    formTitle:' ',
+    formText:' ',
     showModel:false,
     tempFilePaths: '',
-    chan: []
+    chan: [],
+    imageHeight:0,
+    imageWidth:0,
+    currentItemHeight:0,
+    leftH:0,
+    rightH:0
   },
-
+  /*添加内容图标按钮*/
   add(){
     var showModel=this.data.showModel
     var that=this
@@ -96,9 +66,16 @@ Page({
       })
     }
   },
-
+  ShowContent:function(e){
+    var content=JSON.stringify(e.currentTarget.dataset.index)
+    console.log("content",content)
+    wx.navigateTo({
+      url: "DetailContent/DetailContent?content=" + content,
+    })
+  },
   chooseimage: function () {  
     var tempFilePaths=this.data.tempFilePaths
+    var that = this; 
     if(tempFilePaths.length==0){
       wx.chooseImage({  
         sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
@@ -107,7 +84,14 @@ Page({
           // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
           this.setData({  
             tempFilePaths:res.tempFilePaths  
-          })  
+          })
+          wx.getImageInfo({
+            src: that.data.tempFilePaths[0],
+            success: function (res) {
+              that.data.imageHeight=res.height
+              that.data.imageWidth=res.width
+            }
+          })
         }  
       })  
     }
@@ -170,37 +154,62 @@ Page({
     console.log(this.data.chan)
   },
 
+  formSubmit:function(e){     //添加与存储
+    let{formTitle, formText}=e.detail.value
+    if(!formTitle){
+      wx.showToast({
+        title: '标题不能为空',
+        icon:'none'
+      })
+    }else if(!formText){
+      wx.showToast({
+        title: '文字不能为空',
+        icon:'none'
+      })
+    }else if(!this.data.tempFilePaths){
+      wx.showToast({
+        title: '图片不能为空',
+        icon:'none'
+      })
+    }else{
+      var add={
+        "Cover": this.data.tempFilePaths[0],
+        "Title":formTitle,
+        "Text":formText,
+        "CoverHeight": this.data.imageHeight,
+        "CoverWidth": this.data.imageWidth
+      }
+      this.data.noramalData.push(add)
+      this.CalculateImage()
+    }
+  },
 
+  CalculateImage:function(){
+    var that = this;
+    var allData = that.data.noramalData;
+    for (let i = 0; i < allData.length; i++) {
+      var height=parseInt(Math.round(allData[i].CoverHeight * 370 / allData[i].CoverWidth));
+      if(height){
+        //allData[i].ShowHeight=allData[i].CoverHeight
+        var currentItemHeight = parseInt(Math.round(allData[i].CoverHeight * 370 / allData[i].CoverWidth));
+        allData[i].CoverHeight = currentItemHeight + "rpx";//因为xml文件中直接引用的该值作为高度，所以添加对应单位
+        if (that.data.leftH == that.data.rightH || that.data.leftH < that.data.rightH) {//判断左右两侧当前的累计高度，来确定item应该放置在左边还是右边
+          that.data.leftList.push(allData[i]);
+          that.data.leftH += currentItemHeight;
+        } else {
+          that.data.rightList.push(allData[i]);
+          that.data.rightH += currentItemHeight;
+        }
+      }
+    }
+    that.setData({
+      leftList: that.data.leftList,
+      rightList: that.data.rightList,
+    })
+  },
 
     //以本地数据为例，实际开发中数据整理以及加载更多等实现逻辑可根据实际需求进行实现   
   onLoad: function(options) {
-    var that = this;
-    var allData = that.data.noramalData;
-    //定义两个临时的变量来记录左右两栏的高度，避免频繁调用setData方法
-    var leftH = 0;
-    var rightH = 0;
-    var leftData = [];
-    var rightData = [];
-    for (let i = 0; i < allData.length; i++) {
-      var currentItemHeight = parseInt(Math.round(allData[i].CoverHeight * 370 / allData[i].CoverWidth));
-      console.log("currentItemHeight233",currentItemHeight)
-      console.log("leftH",leftH)
-      console.log("rightH",rightH)
-      allData[i].CoverHeight = currentItemHeight + "rpx";//因为xml文件中直接引用的该值作为高度，所以添加对应单位
-      console.log("allData[i].CoverHeight",allData[i].CoverHeight)
-      if (leftH == rightH || leftH < rightH) {//判断左右两侧当前的累计高度，来确定item应该放置在左边还是右边
-        leftData.push(allData[i]);
-        leftH += currentItemHeight;
-      } else {
-        rightData.push(allData[i]);
-        rightH += currentItemHeight;
-      }
-    }
-    //更新左右两栏的数据以及累计高度
-    that.setData({
-      leftList: leftData,
-      rightList: rightData,
-      Title:'标题标题标题标题标题标题',
-      Text:'文字文字文字文字文字文字'
-    })
-  },})
+    this.CalculateImage()
+  },
+})
