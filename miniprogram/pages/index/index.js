d0cd365413b4b3f71fe88a1ec10e48d1c4c13455
yangziteng <1620842362@qq.com>
2021-12-06 10:08:07 +0800

    下拉bug修复

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -10,6 +10,15 @@ Page({
       day: new Date().getDay(),
     },
   },
+  // onPullDownRefresh(){
+  //   wx.showNavigationBarLoading() //在标题栏中显示加载
+  //   console.log("下拉刷新")
+  //   setTimeout( function() {
+  //     wx.hideNavigationBarLoading() //完成停止加载
+  //     wx.stopPullDownRefresh() //停止下拉刷新
+  //   },1500);
+  //   console.log("over")
+  // },
   async onLoad() {
     var that = this;
     var args = wx.getStorageSync('args')
