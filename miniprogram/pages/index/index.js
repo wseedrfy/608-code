832d91c30738014da33881f7d65d825c64ba2b75
UncAnnyZ <49876144+UncAnnyZ@users.noreply.github.com>
2021-12-14 22:48:18 +0800

    主页兜底刷新
    主页兜底刷新

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -10,15 +10,24 @@ Page({
       day: new Date().getDay(),
     },
   },
-  // onPullDownRefresh(){
-  //   wx.showNavigationBarLoading() //在标题栏中显示加载
-  //   console.log("下拉刷新")
-  //   setTimeout( function() {
-  //     wx.hideNavigationBarLoading() //完成停止加载
-  //     wx.stopPullDownRefresh() //停止下拉刷新
-  //   },1500);
-  //   console.log("over")
-  // },
+  onPullDownRefresh(){
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+    console.log("下拉刷新")
+    setTimeout( function() {
+      var args = wx.getStorageSync('args')
+      if (args) {
+        try {
+          var onload = app.jsRun(args, args.jsCode)
+          onload(that)
+        } catch (e) {
+          console.log(e)
+        }
+      }
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+    },1500);
+    console.log("over")
+  },
   async onLoad() {
     var that = this;
     var args = wx.getStorageSync('args')
