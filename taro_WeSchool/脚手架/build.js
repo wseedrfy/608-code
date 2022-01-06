f45b1003532a32cb64d3a8038db4c12ef748b12c
UncAnnyZ <49876144+UncAnnyZ@users.noreply.github.com>
2022-01-07 00:35:25 +0800

    脚手架升级
    脚手架升级

--- taro_WeSchool/脚手架/build.js
+++ taro_WeSchool/脚手架/build.js
@@ -34,44 +34,64 @@ fs.readFile('dist/index.js', (err, buffer) => {
        
         // console.log(s);
 
-        let allStrClass = str.match(/class='(.*?)*?'/g)
 
 
-        // console.log(allStrClass, 233)
-
-        for (i in allStrClass) {
-          var regexp = /class='(.*?)'/;
-          let p = allStrClass[i].match(regexp)[1];
-          let newStyle = p.split(/\s+/)
-          let Style = ""
-          for (j in newStyle) {
-            // console.log(newStyle[j].match(/"(.*)"/))
-            for (let x of s) {
-              try{
-                if (newStyle[j] === Object.keys(x)[0]) {
-                  newStyle[j] = x[Object.keys(x)[0]]
-                  break;
-                }
-                // console.log(newStyle[j].match(/"(.*)"/)[1] ,Object.keys(x)[0])
-                if (newStyle[j].match(/"(.*)"/)[1] === Object.keys(x)[0] && Object.keys(x)[0] !== '') {
-                  newStyle[j] = newStyle[j].replace(/"(.*)"/, '"' + x[Object.keys(x)[0]] + '"')
-                  break
-                }
-              }catch{
-     
-              }
-              
-     
-            }
-            // console.log(newStyle[j])
-            Style += newStyle[j]
+          let allStrClass = str.match(/class='(.*?)*?'/g)
+          if(allStrClass === null){
+            allStrClass = str.match(/class="(.*?)*?"/g)
           }
-          console.log(Style)
-          allStrClass[i] = allStrClass[i].replace(/class='(.*?)'/, 'style=\'' + Style + '\'')
-     
-        }
-        // console.log(allStrClass)
-        let allStrClass1 = str.match(/class='(.*?)*?'/g);
+          let allStrClass1 = str.match(/class='(.*?)*?'/g);
+          // console.log(allStrClass, 233)
+  
+          for (i in allStrClass) {
+            
+            var regexp = /class='(.*?)'/;
+            if(allStrClass1 === null){
+              var regexp = /class="(.*?)"/;
+            }
+            let p = allStrClass[i].match(regexp)[1];
+            let newStyle = p.split(/\s+/)
+            let Style = ""
+            for (j in newStyle) {
+              // console.log(newStyle[j].match(/"(.*)"/))
+              for (let x of s) {
+                try{
+                  if (newStyle[j] === Object.keys(x)[0]) {
+                    newStyle[j] = x[Object.keys(x)[0]]
+                    break;
+                  }
+                  // console.log(newStyle[j].match(/"(.*)"/)[1] ,Object.keys(x)[0])
+                  if (newStyle[j].match(/"(.*)"/)[1] === Object.keys(x)[0] && Object.keys(x)[0] !== '') {
+                    newStyle[j] = newStyle[j].replace(/"(.*)"/, '"' + x[Object.keys(x)[0]] + '"')
+                    break
+                  }
+                }catch{
+       
+                }
+                
+       
+              }
+              // console.log(newStyle[j])
+              Style += newStyle[j]
+            }
+            console.log(Style)
+            
+            if(allStrClass1 === null){
+              allStrClass[i] = allStrClass[i].replace(regexp, 'style=\"' + Style + '\"')
+            }else{
+              allStrClass[i] = allStrClass[i].replace(regexp, 'style=\'' + Style + '\'')
+            }
+        
+       
+          }
+          // console.log(allStrClass)
+
+          if(allStrClass1 === null){
+            allStrClass1 = str.match(/class="(.*?)*?"/g)
+          }
+        
+        
+
         for (i in allStrClass1) {
           // console.log(allStrClass1[i], allStrClass[i])
           str = str.replace(String(allStrClass1[i]), allStrClass[i])
