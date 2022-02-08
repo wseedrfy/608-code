b55fe79b95a625ca5a6fe9f9446099523e08ea7b
Minf <287728237@qq.com>
2022-02-08 23:55:51 +0800

    课表修改、去除open-data

--- miniprogram/pages/curriculum/curriculum.js
+++ miniprogram/pages/curriculum/curriculum.js
@@ -98,14 +98,14 @@ Page({
       mediaType: ['image'],
       sourceType: ['album'],
       success(res) {
-        console.log(res)
-        console.log(res.tempFiles[0].tempFilePath,"临时本地地址");
+        // console.log(res)
+        // console.log(res.tempFiles[0].tempFilePath,"临时本地地址");
 
         let fs = wx.getFileSystemManager();
         let FilePath = fs.saveFileSync(res.tempFiles[0].tempFilePath);
 
-        console.log(fs.getFileInfo(FilePath),"文件信息"); 
-        console.log(FilePath,"本地缓存地址");
+        // console.log(fs.getFileInfo(FilePath),"文件信息"); 
+        // console.log(FilePath,"本地缓存地址");
         // console.log(FileManager.readFileSync(FilePath)); 
         wx.showLoading({
           title: '处理中...',
