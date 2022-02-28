c5f675d83b41fc6f4027c87561f04722992e0e83
1281203681@qq.com <1281203681@qq.com>
2022-02-28 20:13:56 +0800

    报名框架

--- miniprogram/pages/more/pages/Freshman/Freshman.js
+++ miniprogram/pages/more/pages/Freshman/Freshman.js
@@ -5,11 +5,54 @@ Page({
    * 页面的初始数据
    */
   data: {
-    content: {
-      ShowHeight: 200,
-      AllPhoto: {
-        "url": "https://z3.ax1x.com/2021/10/15/58cq9e.png"
-      }
+    content:{
+      AllPhoto:[
+        "cloud://cloud1-6gtqj1v4873bad50.636c-cloud1-6gtqj1v4873bad50-1307814679/1644733579000.png"
+      ],
+      Cover:"cloud://cloud1-6gtqj1v4873bad50.636c-cloud1-6gtqj1v4873bad50-1307814679/1644733579000.png",
+      CoverHeight:"1792rpx",
+      CoverWidth:828,
+      Label:"社团招新",
+      School:"广东石油化工学院",
+      ShowHeight:1792,
+      Title:"xxx招新啦",
+      association:[
+        {
+          detial:"biubb创业实践社",
+          name:"社团/机构"
+        },
+        {
+          detial:"15916513671",
+          name:"联系方式"
+        },
+        {
+          detial:"张三",
+          name:"联系人"
+        },
+        {
+          detial:"20114340331",
+          name:"学号"
+        },
+      ],
+      endTime:"2022-05-07",
+      question:[
+        {
+          content:"姓名",
+          id:1644642857000
+        },
+        {
+          content:"学号",
+          id:1644642857000
+        },
+        {
+          content:"其它部门",
+          id:1644642857000
+        },
+        {
+          content:"是否服从调剂",
+          id:1644642857000
+        },
+      ]
     }
   },
 
