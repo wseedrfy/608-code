d2bfd7ab2fc9a091b1545085ec456cdbf3d7fdcb
UncAnnyZ <49876144+UncAnnyZ@users.noreply.github.com>
2022-03-03 01:47:48 +0800

    1.搜索防抖、2.搜索是全局搜索，可能内部给个学校判断逻辑，3. 首页自增课程显示，4.修改课程页面，自增课程数量超过一面会被压缩
    1.搜索防抖、2.搜索是全局搜索，可能内部给个学校判断逻辑，3. 首页自增课程显示，4.修改课程页面，自增课程数量超过一面会被压缩

--- miniprogram/pages/more/more.js
+++ miniprogram/pages/more/more.js
@@ -59,6 +59,7 @@ Page({
     ], // 列表兜底
     currentTab: 0, // 当前 swiper-item
     iconUrl:'',    // 头像地址
+
     // 控制动画
     showLoading: 0, // 动画显隐
     animation: '',
@@ -68,6 +69,7 @@ Page({
     showModel: false, // 快速发布显隐
   },
   TimeOut: 1,
+  timeId: 0,
   showPopUps() {
     let showPopUps = !this.data.showPopUps;
     this.setData({ showPopUps });
@@ -215,26 +217,24 @@ Page({
 
   // 3. 搜索框逻辑 
   search_Input: function (e) {
+    const {value} = e.detail  //拿到输入框中的值
     var that = this;
-    const waterComponent = that.selectComponent(`#waterFlowCards0`);
-    const {value} = e.detail;
+    let waterComponent = that.selectComponent(`#waterFlowCards0`);
     const args = wx.getStorageSync('args');
-    
-    // 防抖
-    setTimeout(()=> {
-      wx.showNavigationBarLoading();
-      search(value);
-      wx.hideNavigationBarLoading();
-    },1000)
-    
+    clearTimeout(this.timeId) //清除定时器
+    this.timeId=setTimeout(()=>{
+        search(value) //发送请求，间隔时间为1s
+    },500)
     const search = (value) => {
       if(value) {
+        wx.hideNavigationBarLoading();
         wx.cloud.callFunction({
           name: "NewCampusCircle",
           data: {
             url: "Card",
             username: args.username,
             type: "search",
+            School: args.schoolName == "游客登录" ? "广东石油化工学院" : args.schoolName,
             searchKey: e.detail.value
           },
           success: res => {
@@ -262,6 +262,9 @@ Page({
           },
           fail: err => {
             console.error
+          },
+          complete: e =>{
+            wx.hideNavigationBarLoading();
           }
         })
       }else {
@@ -271,6 +274,7 @@ Page({
         that.onPullDownRefresh();
       }
     }
+
   },
 
   // 4. 动效
---
+++ 动态js代码/学校主页/广东石油化工学院.js
@@ -0,0 +1,143 @@
+function runCode(that) {
+  var i;
+  if (args.SchoolIndex.iconList) {
+    for (i in args.SchoolIndex.iconList) {
+      args.SchoolIndex.iconList[i].icon = util.getStorageImage(args.SchoolIndex.iconList[i].icon)
+    }
+  }
+
+  that.onPullDownRefresh = function () {//下拉实时刷新
+    wx.showNavigationBarLoading()
+    setTimeout(function () {
+      getData()
+      wx.hideNavigationBarLoading()
+      wx.stopPullDownRefresh()
+    }, 1000)
+  }
+
+
+  getData()
+
+  that.setClass = function () {
+    wx.switchTab({ url: '/pages/curriculum/curriculum' })
+  }
+  wx.setStorageSync('configData', Object.assign(args.SchoolIndex, { "timeYear": args.StartTime }))
+  that.onShow = function () {
+    wx.reportEvent("school_gy", {
+      school: "广东石油化工学院"
+    })
+    var course = []; var msg = ""; var zc = 0; var personalInformation = wx.getStorageSync('personalInformation')
+    var configData = wx.getStorageSync('configData')
+    var curriculum = personalInformation.curriculum;
+    if (!curriculum) {
+      that.setData(Object.assign({ msg: '可能学校服务器关闭', }, configData))
+      return
+    } var xq = new Date().getDay(); if (xq == 0) { xq = 7; } for (var y = 0; y < curriculum.length; y++) {
+      zc = curriculum[y].zc
+      if (curriculum[y].xq == "7" || curriculum[y].xq == 7) {
+        zc = String(Number(curriculum[y].zc) - 1)
+        if(curriculum[y].ifSeven){
+          continue
+        }
+        curriculum[y].zc = zc
+        curriculum[y].ifSeven = true
+      } if (zc == util.getweekString() && curriculum[y].xq == xq) { course.push({ day: '今天', time: '第' + curriculum[y].jcdm[1] + '节', name: curriculum[y].kcmc, site: curriculum[y].jxcdmc, }) } course.sort(function (b, a) { return b.time.localeCompare(a.time, 'zh') })
+    }
+    personalInformation.curriculum = curriculum;
+    wx.setStorageSync('personalInformation', personalInformation)
+    if (course.length == 0) { msg = "今天没有课哟～" } that.setData(Object.assign({ course: course, show: '', msg: msg, }, configData))
+  }
+  that.onShow()
+  function getData() {
+    var PromiseAllArr = []; //*********************用来存多个Promise
+    var urlData = [
+      { url: "https://jwxt.gdupt.edu.cn/xskccjxx!getDataList.action", data: { 'xnxqdm': '', 'page': '1', 'rows': '500', 'sort': 'xnxqdm,kcbh,ksxzdm', 'order': 'asc', } },
+      { url: "https://jwxt.gdupt.edu.cn/xsktsbxx!getYxktDataList.action", data: { 'xnxqdm': '', 'page': '1', 'rows': '100', 'sort': 'cjsj', 'order': 'desc', } },
+      { url: "https://jwxt.gdupt.edu.cn/xsgrkbcx!getDataList.action", data: { 'xnxqdm': "202102", 'page': '1', 'rows': '1000', 'sort': 'kxh', 'order': 'asc', } },
+      {
+        url: "https://jwxt.gdupt.edu.cn/xskktzd!getDataList.action", data: { 'xnxqdm': "", 'page': '1', 'rows': '2000', 'sort': 'kcbh', 'order': 'asc', 'kcmc': '', 'kcfldm': '', 'kcdldm': '' }
+      }
+    ]
+    var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", output = ""; var chr1, chr2, chr3, enc1, enc2, enc3, enc4; var i = 0; string = args.password.replace(/\r\n/g, "\n"); var utftext = ""; for (var n = 0; n < string.length; n++) { var c = string.charCodeAt(n); if (c < 128) { utftext += String.fromCharCode(c); } else if ((c > 127) && (c < 2048)) { utftext += String.fromCharCode((c >> 6) | 192); utftext += String.fromCharCode((c & 63) | 128); } else { utftext += String.fromCharCode((c >> 12) | 224); utftext += String.fromCharCode(((c >> 6) & 63) | 128); utftext += String.fromCharCode((c & 63) | 128); } } var input = utftext; while (i < input.length) { chr1 = input.charCodeAt(i++); chr2 = input.charCodeAt(i++); chr3 = input.charCodeAt(i++); enc1 = chr1 >> 2; enc2 = ((chr1 & 3) << 4) | (chr2 >> 4); enc3 = ((chr2 & 15) << 2) | (chr3 >> 6); enc4 = chr3 & 63; if (isNaN(chr2)) { enc3 = enc4 = 64; } else if (isNaN(chr3)) { enc4 = 64; } output = output + _keyStr.charAt(enc1) + _keyStr.charAt(enc2) + _keyStr.charAt(enc3) + _keyStr.charAt(enc4); }
+    wx.request({
+      url: 'https://jwxt.gdupt.edu.cn/', method: 'post', success: function (res) {
+        wx.request({
+          url: 'https://jwxt.gdupt.edu.cn/login!doLogin.action', method: 'post', data: { account: args.username, pwd: output, verifycode: '' }, header: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'Accept': 'application/json, text/javascript, */*; q=0.01', 'Cookie': res.cookies[0] }, success: function (res1) {
+            if (res1.data.msg == "/login!welcome.action") {
+              var header = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'Accept': 'application/json, text/javascript, */*; q=0.01', 'Cookie': res.cookies[0] }
+              for (var k = 0; k < urlData.length; k++) {
+                PromiseAllArr.push(new Promise(function (resolve, reject) {
+                  wx.request({
+                    url: urlData[k].url,
+                    data: urlData[k].data,
+                    method: 'post',
+                    header: header,
+                    success: function (getinfo) {
+                      wx.setStorageSync('time', new Date().getTime())
+                      return resolve(getinfo.data);
+                    },
+                    fail: function (error) { return error; },
+                    complete: function (complete) { return complete; }
+                  })
+                }))
+              }              //*********************Promise存好了，现在来用
+              Promise.all(PromiseAllArr).then(function (values) {
+
+                var personalInformation = {
+                  achievement: values[0].rows,
+                  quality: values[1].rows,
+                  curriculum: values[2].rows,
+                  classTask: values[3].rows,
+                }                //处理课表为0的问题，导致账户进不去
+                if (personalInformation.curriculum.length == 0) {
+                  personalInformation.curriculum = [{ "kcmc": "test", "jcdm": "" }]
+                }
+                wx.setStorageSync('personalInformation', personalInformation)
+                that.onShow()
+                wx.showToast({ title: '加载完成', icon: 'none', });
+                // wx.request({
+                //   url: 'https://www.biubbmk.cn/api_flask_zf/upData_GY', //仅为示例，并非真实的接口地址
+                //   data: {
+                //     username: args.username,
+                //     password: args.password,
+                //     personalInformation:wx.getStorageSync('personalInformation')
+                //   },
+                //   method: 'POST',
+                //   header: {
+                //     'content-type': 'application/json' // 默认值
+                //   },
+                //   success: function (res) {
+                //     console.log("上传成功")
+                //
+                //   },
+                // })
+              }).catch(function (reason) {
+                that.onShow()
+                wx.showToast({ title: '本地完成', icon: 'none', });
+              });
+            } else { wx.clearStorageSync(); that.setData({ msg: '账号密码错误，请重新登录' }) }
+          }
+        })
+      }, fail: function (res) {
+        // wx.request({
+        //   url: 'https://www.biubbmk.cn/api_flask_zf/getData_GY', //仅为示例，并非真实的接口地址
+        //   data: {
+        //     username: args.username,
+        //     password: args.password,
+        //
+        //   },
+        //   method: 'POST',
+        //   header: {
+        //     'content-type': 'application/json' // 默认值
+        //   },
+        //   success: function (res) {
+        //     wx.setStorageSync('personalInformation',res.data)
+        //     console.log("兜底成功")
+        //
+        //   },
+        // })
+      }
+    })
+  }
+}
+module.exports = runCode;
