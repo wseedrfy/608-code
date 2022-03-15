e73e85c9cbe3e1f83c7c7730db8e0b95913998e7
Minf <287728237@qq.com>
2022-03-15 14:45:07 +0800

    game

--- cloudfunctions/NewCampusCircle/Card/index.js
+++ cloudfunctions/NewCampusCircle/Card/index.js
@@ -8,12 +8,17 @@ exports.main = async (event) => {
     msg: 'error'
   }
   switch (event.type) {
+    // 校园圈主页读取卡片
     case "read":
       data = await read(event); //  
       break;
+    // 校园圈主页搜索逻辑
     case "search":
       data = await search(event); //  
       break;
+    // 电子游戏板块读取卡片
+    case "read_Game":
+      data = await read_Game(event)
   }
   return data
 }
@@ -67,4 +72,32 @@ async function search(event) {
   } catch (e) {
     return e
   }
+}
+
+async function read_Game(event) {
+  var skipPage = event.currentPage * 10;
+  var obj = {
+    School: event.School
+  }
+  event.ShowId != "全部" ? obj["Label"] = event.ShowId : '';
+  console.log(event);
+  try {
+
+    // if(event.School === '茂名职业技术学院'){
+    //   return await db.collection('Campus-Circle').orderBy('indexFront', 'desc').orderBy('Star', 'desc').where(obj).skip(skipPage).limit(10).get();
+    // }
+    return await db.collection('Campus-Circle').field({
+      'CommentList.InputComment': false,
+      'CommentList.CommentTime': false,
+      'CommentList.iconUser': false,
+      'CommentList.nickName': false,
+      'CommentList.username': false,
+      'Star_User.Star_time': false,
+      'Star_User.iconUrl': false,
+      'Star_User.nickName': false,
+    }).orderBy('indexFront', 'desc').orderBy('SortTime', 'desc').orderBy('Time', 'desc').where(obj).skip(skipPage).limit(15).get();
+  } catch (e) {
+    console.error(e);
+  }
+
 }
