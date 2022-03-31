556ad38cc621d8f143283af1a53237d409322573
张粤 <210327422@qq.com>
2022-03-31 19:06:30 +0800

    广东理工的conwork加密

--- we校园爬虫/another/conwork.py
+++ we校园爬虫/another/conwork.py
@@ -9,20 +9,40 @@ def encodeInp(input):
     while True:
         char1 = ord(input[i])
         i += 1
-        char2 = ord(input[i])
+        if i < len(input):
+            char2 = ord(input[i])
+        else:
+            char2 = 'nan'
+
         i += 1
-        char3 = ord(input[i])
+        if i < len(input):
+            char3 = ord(input[i])
+        else:
+            char3 = 'nan'
         i += 1
         enc1 = char1 >> 2
-        enc2 = ((char1 & 3) << 4) | (char2 >> 4)
-        enc3 = ((char2 & 15) << 2) | (char3 >> 6)
-        enc4 = char3 & 63
-        if math.isnan(char2):
+        if char2 != 'nan':
+            enc2 = ((char1 & 3) << 4) | (char2 >> 4)
+        else:
+            enc2 = (char1 & 3) << 4
+        if char2 != 'nan' and char3 != 'nan':
+            enc3 = ((char2 & 15) << 2) | (char3 >> 6)
+        elif char2 != 'nan' and char3 == 'nan':
+            enc3 = (char2 & 15) << 2
+        else:
+            enc3 = 0
+        if char3 != 'nan':
+            enc4 = char3 & 63
+        else:
+            enc4=0
+        if char2=='nan':
             enc3 = 64
             enc4 = 64
-        elif math.isnan(char3):
+        elif char3=='nan':
             enc4 = 64
         output = output + keyStr[enc1] + keyStr[enc2] + keyStr[enc3] + keyStr[enc4]
         if i >= len(input):
             break
     return output
+
+
